89f984ae95d5deaa7c5d85fead3a018b8cb8b317
Alexander Iadarola <alexanderiadarola@gmail.com>
2017-10-24 20:30:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,3 @@
 function happyHolidays(string) {
   return ("Happy holidays!")
 }
-function happyHolidaysTo(string) {
-  return(`Happy holidays, you!`)
-}
-function happyHolidayTo(string) {
-  return("Happy Independence Day, you!")
-}
-function holidayCountdown(string) {
-  return("It\'s 20 days until Mother\'s Day!")
-}
